a17463f44bfbd6de221b6a64cd945c1c4f9ad651
elena-miles <elena.airey@gmail.com>
2024-09-19 16:17:00 +0200

    corrections

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 function generatePoem(event) {
-event.preventDefault();
-
-
-new Typewriter('#poem', {
-    strings: ["la tombe dit a la rose"],
-    autoStart: true,
-    delay: 1, 
-    cursor:0, 
+  event.preventDefault();
+  document.querySelector('#poem').innerHTML = "";
+  
+  new Typewriter('#poem', {
+      strings: ["la tombe dit a la rose"], 
+      autoStart: true,
+      delay: 1, 
+      cursor: null, 
   });
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form"); 
-poemFormElement.addEventListener("submit", generatePoem); 
-
+poemFormElement.addEventListener("submit", generatePoem);
