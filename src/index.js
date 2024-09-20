00014333db5a1f23aa63f51dc0c533cc5a39071b
elena-miles <elena.airey@gmail.com>
2024-09-20 10:48:38 +0200

    final changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 function generatePoem (event) {
     event.preventDefault();
     
-    let boxElement = document.querySelector(".poem-text-box");
-    boxElement.innerHTML = "you are a massive dork"; 
+    //let boxElement = document.querySelector(".poem-text-box");
+    //boxElement.innerHTML = "you are a massive dork"; 
+    new Typewriter(".poem-text-box", {
+        strings: ["Hey, YOU. What do you want now?"],
+        autoStart: true,
+        delay: 70, 
+        cursor: null
+    });
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form"); 
